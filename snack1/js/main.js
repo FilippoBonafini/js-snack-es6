bfb0401854e554ddd6435b3be8b627d3e610870c
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-03-08 21:18:26 +0100

    Finita grafica snack 1

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -22,13 +22,28 @@ console.log(invitati);
 console.log(tavolo);
 
 invitati.forEach((element, index) => {
-    const person = [{
+    const person = {
         nome: element,
         tavolo: tavolo,
         posto: index + 1
-    }];
+    };
     elencoInvitati.push(person);
     console.log("Nel " + tavolo + " c'è " + element + ' al posto ' + (index + 1));
 });
 
 console.log(elencoInvitati);
+
+
+const template = document.getElementById('invitatiTemplate');
+const ulContainerInvitati = document.getElementById('listaInvitati');
+
+for (let i = 0; i < elencoInvitati.length; i++) {
+    const invitato = elencoInvitati[i];
+    const invitatoTemplate = template.content.cloneNode(true);
+
+    invitatoTemplate.querySelector('.tavolo').innerHTML = invitato.tavolo;
+    invitatoTemplate.querySelector('.posto').innerHTML = invitato.posto;
+    invitatoTemplate.querySelector('.nome').innerHTML = invitato.nome;
+
+    ulContainerInvitati.append(invitatoTemplate);
+}
